1ff0b3d1fb4e0361ec333f07820ee9b26822598a
mask ser <444shijil@mail.com>
2024-05-30 00:44:14 +0530

    added mstatus

--- plugins/anivideo.js
+++ plugins/anivideo.js
@@ -4,7 +4,7 @@ izumi ({
     fromMe: mode,
     desc: "random Naruto anime videos",
     type: "AnimeVideo",
-}, async (message, match, client) => {
+}, async (message, match) => {
 const { result } = await getJson('https://api.maskser.me/api/anime/naruto')
 message.sendFromUrl(result.url,{caption: "Iᴢᴜᴍɪ"})
 });
@@ -14,7 +14,16 @@ izumi ({
     fromMe: mode,
     desc: "random  anime videos",
     type: "AnimeVideo",
-}, async (message, match, client) => {
+}, async (message, match) => {
 const { result } = await getJson('https://api.maskser.me/api/anime/anivideo')
 message.sendFromUrl(result.url,{caption: "Iᴢᴜᴍɪ"})
+});
+ izumi ({
+    pattern: "mstatus$",
+    fromMe: mode,
+    desc: "random Malayalam status videos",
+    type: "media",
+}, async (message, match) => {
+const { result } = await getJson('https://api.maskser.me/api/randomvideo/msts')
+message.sendFromUrl(result.video,{caption: "Iᴢᴜᴍɪ"})
 });
